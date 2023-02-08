d71f33a1f56f843f9841c84bd1de390cc4598e45
Onurcan Sever <onsever@icloud.com>
2023-02-08 10:46:53 -0500

    initial commit 2

---
+++ src/utilities/Colors.ts
@@ -0,0 +1,5 @@
+export default class Colors {
+  static primary = '#FF0000';
+  static secondary = '#00FF00';
+  static tertiary = '#0000FF';
+}
---
+++ src/utilities/Fonts.ts
@@ -0,0 +1,5 @@
+export default class Fonts {
+  static primary = 'Roboto';
+  static secondary = 'Helvetica';
+  static tertiary = 'Arial';
+}
