556c4e0a23737027ab640f5e9af7a749bb3c7d2c
Onurcan Sever <onsever@icloud.com>
2023-03-08 18:22:02 -0500

    reviews page completed

--- src/screens/readreviews/index.tsx
+++ src/screens/readreviews/index.tsx
@@ -5,6 +5,7 @@ import {NativeStackScreenProps} from '@react-navigation/native-stack';
 import {HomeStackNavigatorProps} from '../../navigators/HomeStackNavigator';
 import ReviewCard from '../../components/ReviewCard';
 import {SafeAreaView} from 'react-native-safe-area-context';
+import {supabase} from '../../utilities/Supabase';
 
 type ReadReviewsScreenProps = NativeStackScreenProps<
   HomeStackNavigatorProps,
@@ -14,14 +15,34 @@ export default function ReadReviewsScreen({
   navigation,
   route,
 }: ReadReviewsScreenProps) {
+  const [reviews, setReviews] = React.useState<any[]>([]);
   const movie = route.params.movie;
 
-  console.log(movie);
   React.useLayoutEffect(() => {
     navigation.setOptions({
       title: 'Read Reviews',
     });
   }, [navigation]);
+
+  React.useEffect(() => {
+    const fetchData = async () => {
+      const {data: reviewData, error: reviewError} = await supabase
+        .from('reviews')
+        .select('*')
+        .eq('movie_id', movie.id);
+
+      if (reviewError) {
+        console.log('error', reviewError);
+        return;
+      }
+
+      setReviews(reviewData);
+    };
+
+    fetchData();
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   return (
     <SafeAreaView>
@@ -34,17 +55,15 @@ export default function ReadReviewsScreen({
         <Text style={styles.description}>Release date: May 4, 2012</Text>
 
         <View style={styles.reviewsContainer}>
-          <ReviewCard
-            image={require('../../../assets/profile.png')}
-            title="Daniel Miolan"
-            description="Great movie! I highly suggest it! "
-          />
-
-          <ReviewCard
-            image={require('../../../assets/profile.png')}
-            title="Sandeep Kaur"
-            description={'I would not recommend it. '}
-          />
+          <Text style={styles.reviewsTitle}>Reviews</Text>
+          {reviews.map((review, index) => (
+            <ReviewCard
+              key={index}
+              image={require('../../../assets/profile.png')}
+              title={review.user_name || 'Anonymous'}
+              description={review.review}
+            />
+          ))}
         </View>
       </View>
     </SafeAreaView>
